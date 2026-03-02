a0ae395b5d479d936f9d51120188b5a8291a3292
GiraFFik <a.zinger2003@gmail.com>
2026-03-02 23:34:33 +0500

    fix bugs

--- src/routes/subscription.routes.ts
+++ src/routes/subscription.routes.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { purchaseSubscription } from "../controllers/subscription.controller"; // Убрали getSubscription
+import { getSubscription, purchaseSubscription } from "../controllers/subscription.controller";
 
 const router = Router();
 
-// Убрали этот маршрут
-// router.get("/:telegramId", getSubscription);
-
+router.get("/:telegramId", getSubscription);
 router.post("/:telegramId/purchase", purchaseSubscription);
 
 export default router;
